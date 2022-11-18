e9218f73b329299bbc160e760725140ab61c6156
Avraham Micael <avrahammicael@protonmail.com>
2022-11-18 18:30:52 -0300

    babel added

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,3 @@
 module.exports = (app, express) => {
-  app.use(express.static(require('path').join(__dirname, '../public')));
+  app.use(express.static(require('path').join(__dirname, '../public/dist')));
 };
